230f48d9df3de5a6e31354314461a0aa0a0e1b84
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-10-02 02:42:14 -0700

    added source

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Router, Route } from 'svelte-routing';
+  import Home from './routes/Home.svelte';
+  import History from './routes/History.svelte';
+</script>
+
+<Router>
+  <Route path="/" component={Home}/>
+  <Route path="/history" component={History}/>
+</Router>
