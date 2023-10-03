92c7a2ceaf049090f2f01c10bbf32e30a7d0ac92
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-10-03 08:03:52 -0700

    Updated store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,7 @@
 import { writable, type Writable } from "svelte/store";
 import { type Auth, type User } from 'firebase/auth';
+import { Firestore } from 'firebase/firestore';
 
+export let database: Writable<Firestore | null> = writable<Firestore| null>(null);
 export let auth: Writable<Auth | null> = writable<Auth | null>(null);
 export let user: Writable<User | null> = writable<User | null>(null);
