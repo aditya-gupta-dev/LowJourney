7383ae1103fc4ca5004242220323cdea54ef4faa
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-10-03 00:11:00 -0700

    Updated Home And History layout

---
+++ src/routes/History.svelte
@@ -0,0 +1,1 @@
+<h1>This is history svelte</h1>
--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -4,6 +4,9 @@
     import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
     import { onMount } from "svelte";
     import { auth, user } from "../store/store";
+    import Button from "../components/Button.svelte";
+    import InputField from "../components/InputField.svelte";
+    import Navbar from "../components/Navbar.svelte";
 
     let app: FirebaseApp | undefined = undefined;
 
@@ -22,14 +25,33 @@
         });
     }
 
-    const signOut = async () => {
-       await $auth?.signOut();
-    }
 </script>
 
 {#if $user}
-    <h1>You are signed in</h1>
-    <button on:click={ signOut }>Sign Out</button>
-{:else}
-    <button on:click={ signIn }>Login</button>
+    <Navbar/>
+    <InputField/>
+{:else} 
+    <div class="container">
+        <div class="container-title">
+            <h3>Login</h3>
+        </div>
+        <Button text="Sign In With Google" onClick={ signIn }/>
+    </div>
 {/if}
+
+<style>
+    h3 {
+        font-family: Arial, Helvetica, sans-serif;
+    }
+    .container-title {
+        display: flex;
+        justify-content: center;
+    }
+    .container {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+</style>
